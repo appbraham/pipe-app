68bbe4e06b2725ffd7b438953e610e75fb7a3cde
Abraham Gutierrez <labraham1986@gmail.com>
2024-03-11 18:26:13 -0500

    first commit

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styles: ``
+})
+export class BasicsPageComponent {
+
+  public nameLower: string = 'abraham';
+  public nameUpper: string = 'ABRAHAM';
+  public fullName: string = 'aBrAhAm GuTiErReZ';
+
+}
